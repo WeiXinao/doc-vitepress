5cd5393ec204a6b0ee4926b00dfd1ffb78f8a3fd
XiaoXin <1632967698@qq.com>
2024-04-06 19:35:01 +0800

    vault backup: 2024-04-06 19:35:01

--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -1,6 +1,6 @@
 export default {
  "/Golang/": [
-    {
+  {
       text: 'Golang',
       items: [
       {
@@ -10,28 +10,28 @@ export default {
             { text: 'GoWeb基础', link: '/Golang/基础/GoWeb' },
             { text: '爬虫', link: '/Golang/基础/Go爬虫' },
             { text: '正则表达式', link: '/Golang/基础/Go基础之正则表达式' }
-          ]
-        }, 
-        {
-          text: '面试',
-          items: [
-            { text: 'panic时返回错误', link: '/Golang/面试/Panic时返回错误' }
-          ]
-        }
-      ] 
-    }
-  ],
-  "/Web/": [
-    {
-      text: 'Web',
-      items: [
-        { text: 'HTTPS证书校验', link: '/Web/Web开发之HTTPS证书校验' }
+        ]
+      }, 
+      {
+        text: '面试',
+        items: [
+          { text: 'panic时返回错误', link: '/Golang/面试/Panic时返回错误' }
+        ]
+      }
+    ] 
+  }
+],
+"/Web/": [
+  {
+  text: 'Web',
+  items: [
+    { text: 'HTTPS证书校验', link: '/Web/Web开发之HTTPS证书校验' }
     ]
-    }
-  ],
-  "/NoSQL/": [
-    {
-      text: '基础', link: '/NoSQL/Redis 基础'
-    }
-  ]
+  }
+],
+"/NoSQL/": [
+  {
+    text: '基础', link: '/NoSQL/Redis 基础'
+  }
+]
 } 
