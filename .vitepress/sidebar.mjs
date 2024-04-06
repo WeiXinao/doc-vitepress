7cfc6bf226bf37ef0d114de454502606063d72cf
XiaoXin <1632967698@qq.com>
2024-04-06 18:05:58 +0800

    4/6

--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -5,7 +5,7 @@ export default {
       items: [
         { text: '基础语法', link: '/Golang/Go基础' },
         { text: 'GoWeb基础', link: '/Golang/GoWeb' },
-        { text: '爬虫', link: '/docs/Golang/Go爬虫' }
+        { text: '爬虫', link: '/Golang/Go爬虫' }
       ]
     }
   ],
