549db1aba8256accf3e43e8818537ed02b432232
XiaoXin <1632967698@qq.com>
2024-04-06 18:52:58 +0800

    vault backup: 2024-04-06 18:52:58

--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -5,7 +5,7 @@ export default {
       items: [
         { text: '基础语法', link: '/Golang/Go基础' },
         { text: 'GoWeb基础', link: '/Golang/GoWeb' },
-        { text: '爬虫', link: '/Golang/Go爬虫' }
+        { text: '爬虫', link: '/Golang/Go爬虫' },
         { text: '正则表达式', link: '/Golang/Go基础之正则表达式' }
       ]
     }
